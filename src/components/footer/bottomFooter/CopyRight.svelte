cf2f5bfdd299a0f41d14fec47f660528d3ff607c
fabianlopezdev <fabianlopez@pm.me>
2023-11-07 12:29:14 -0500

    Footer: Initial setup

---
+++ src/components/footer/bottomFooter/CopyRight.svelte
@@ -0,0 +1,21 @@
+<script>
+  const currentyear = new Date().getFullYear();
+</script>
+
+<small>
+  Web Propietat del Club Bàsquet Granollers. Copyright ©
+  {currentyear}
+  . All rights reserved.
+</small>
+
+<style>
+  small {
+    font-size: var(--fnt-sz-footer);
+  }
+
+  @media (max-width: 1025px) {
+    small {
+      font-size: 0.75rem;
+    }
+  }
+</style>
