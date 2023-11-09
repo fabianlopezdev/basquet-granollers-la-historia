c9ffd66f04f0fb2f2f2f740b539e6ede171323d6
fabianlopezdev <fabianlopez@pm.me>
2023-11-09 10:19:15 -0500

    Season: created line of time line

---
+++ src/components/Timeline.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<div class='line'>
+
+</div>
+<style>
+  .line {
+    position: absolute;
+    width: calc(100% - (var(--pd-x) * 2));
+    bottom: 2rem;
+    height: 0.1875rem;
+     background: linear-gradient(
+      90deg,
+      var(--clr-accent) 35.36%,
+      var(--clr-primary) 68.89%
+    );
+  }
+</style>
