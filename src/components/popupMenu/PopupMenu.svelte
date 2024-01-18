8aeee4ed084799b0e5e39125d5834400b625fdd5
fabianlopezdev <fabianlopez@pm.me>
2024-01-18 11:43:15 -0500

    Seasons: made the popup menu with the info of either jugadors, resultats, or social, a component

---
+++ src/components/popupMenu/PopupMenu.svelte
@@ -0,0 +1,39 @@
+<script>
+    import {closeMenuIcon} from "@assets/icons"
+  import { currentIndex, display } from "src/svelte/stores";
+
+  export let SEASONS;
+
+</script>
+<section class="more-info-menu">
+  <header class="menu-header">
+    <h2>
+      <span style="color: var(--clr-accent)">Jugadors</span> Temporada {SEASONS[
+        $currentIndex
+      ].name}
+    </h2>
+    <button on:click={() => display.set("relats")}>{@html closeMenuIcon}</button
+    >
+  </header>
+</section>
+
+<style>
+    .more-info-menu {
+    position: absolute;
+    height: inherit;
+    width: 100vw;
+    margin: 0;
+    background-color: var(--clr-contrast);
+    z-index: 10;
+  }
+  .menu-header {
+    background-color: var(--clr-primary);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-inline: 7.12rem;
+    height: 8.25rem;
+    margin-top: 1rem;
+    margin-left: 1rem;
+  }
+</style>
--- src/components/seasons/Seasons.svelte
+++ src/components/seasons/Seasons.svelte
@@ -3,11 +3,13 @@
   import Options from "./Options.svelte";
   import Timeline from "./Timeline.svelte";
   import SeasonItem from "./SeasonItem.svelte";
+  import PopupMenu from "../popupMenu/PopupMenu.svelte";
   //Import data
   import { SEASONS_INFO, SEASONS_LAYOUT } from "@data/globalConstants";
   import { currentIndex } from "src/svelte/stores";
   import { display } from "src/svelte/stores";
-
+  import {closeMenuIcon} from "@assets/icons"
+  
   const TOTAL_SEASONS = SEASONS_INFO.length;
 
   let windowWidth;
@@ -24,57 +26,79 @@
 </script>
 
 <svelte:window
-  bind:innerWidth={windowWidth}
-  bind:innerHeight={windowHeight}
-  bind:scrollY={windowScrollY}
+bind:innerWidth={windowWidth}
+bind:innerHeight={windowHeight}
+bind:scrollY={windowScrollY}
 />
 
-<main id="seasons">
-  <div
-    class="seasons-container"
-    style="--totalSeasons: {TOTAL_SEASONS}; transform: {transform}"
-  >
-    {#each SEASONS as season, seasonIndex}
-      <SeasonItem {season} {windowHeight} {windowScrollY} {seasonIndex}/>
-    {/each}
-  </div>
-  <Options />
-  <Timeline
-    {SEASONS_INFO}
-    {windowHeight}
-    {windowScrollY}
-    {windowWidth}
-    bind:animationClass
-  />
-  {#if $display === 'jugadors'}
-    <section class='more-info-menu'>
-
-    </section>
+<section id="seasons">
+  {#if $display !== "relats"}
+  <PopupMenu {SEASONS} />
+    <!-- <section class="more-info-menu">
+      <header class="players-header">
+        <h2><span style="color: var(--clr-accent)">Jugadors</span> Temporada {SEASONS[$currentIndex].name}</h2>
+        <button on:click={() => display.set("relats")}>{@html closeMenuIcon}</button>
+      </header>
+    </section> -->
   {/if}
-</main>
+  <div class="season-wrapper">
+    <div
+    class="seasons-container"
+      style="--totalSeasons: {TOTAL_SEASONS}; transform: {transform}"
+    >
+      {#each SEASONS as season, seasonIndex}
+        <SeasonItem {season} {windowHeight} {windowScrollY} {seasonIndex} />
+      {/each}
+    </div>
+    <Options />
+    <Timeline
+      {SEASONS_INFO}
+      {windowHeight}
+      {windowScrollY}
+      {windowWidth}
+      bind:animationClass
+    />
+  </div>
+</section>
 
 <style>
-  main {
+  #seasons {
     background-color: var(--clr-contrast);
-    height: 100dvh;
     scroll-snap-align: start;
-    background-image: url("/cancha.png");
-    background-position: center;
-    max-width: var(--wd-regular);
-    margin: auto;
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
     position: relative;
+    height: 100dvh;
   }
 
-  .more-info-menu {
+  .season-wrapper {
+    display: flex;
+    height: inherit;
+    flex-direction: column;
+    background-position: center;
+    overflow: hidden;
+    margin: auto;
+    background-image: url("/cancha.png");
+    max-width: var(--wd-regular);
+  }
+
+  /* .more-info-menu {
     position: absolute;
     height: inherit;
-    width: 100%;
-    background-color: red;
-    z-index: 1;
+    width: 100vw;
+    margin: 0;
+    background-color: var(--clr-contrast);
+    z-index: 10;
   }
+  
+  .players-header {
+    background-color: var(--clr-primary);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-inline: 7.12rem;
+    height: 8.25rem;
+    margin-top: 1rem;
+    margin-left: 1rem;
+  } */
   .seasons-container {
     width: calc(100% * var(--totalSeasons));
     height: calc(100dvh - var(--pd-y-options-btns) - 2.12rem);
