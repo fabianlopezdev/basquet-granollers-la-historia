ee88f5656300965372e8f6094365375f69980500
fabianlopezdev <fabianlopez@pm.me>
2024-01-22 16:09:26 -0500

    Header: ResponsiveHeader: DropdownMenu: created interaction. If hamburger menu is touched it opens and closes the menu

--- src/components/header/HeaderResponsive.svelte
+++ src/components/header/HeaderResponsive.svelte
@@ -1,16 +1,29 @@
 <script>
-  import {hamburgerMenuIcon} from "@assets/icons";
+  import { hamburgerMenuIcon } from "@assets/icons";
   // export let navigation;
   // export let headerSponsors;
+  let isDropDownMenuOpen = false;
 
+  $: {
+    if (isDropDownMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
 </script>
 
-<button class="dropdown-menu">
+<button
+  class="dropdown-menu-button"
+  on:click={() => (isDropDownMenuOpen = !isDropDownMenuOpen)}
+>
   {@html hamburgerMenuIcon}
 </button>
 
+<ul class:visible={isDropDownMenuOpen} class="dropdown-menu-content"></ul>
+
 <style>
-  .dropdown-menu {
+  .dropdown-menu-button {
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
@@ -20,8 +33,31 @@
     cursor: pointer;
     z-index: 100;
   }
-    @media (max-width: 648px) {
-    .dropdown-menu {
+
+  .dropdown-menu-content {
+    position: fixed;
+    display: flex;
+    background-color: var(--clr-primary);
+    color: var(--clr-contrast);
+    top: calc(var(--hg-header) + var(--hg-separator-line-responsive) );
+    left: 0;
+    right: 0;
+    bottom: 0;
+    padding-inline: var(--pd-x-medium);
+    padding-block: 2.06rem;
+    flex-direction: column;
+    gap: 1.87rem;
+    transform: translateX(100%);
+    transition: transform 0.2s ease-in-out;
+    overflow: auto;
+  }
+
+  .dropdown-menu-content.visible {
+    transform: translateX(0);
+  }
+
+  @media (max-width: 648px) {
+    .dropdown-menu-button {
       right: var(--pd-x-small);
     }
   }
