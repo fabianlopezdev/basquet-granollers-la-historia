ffc8f41d5ab5d0646b9fc43b53f32355f4f41ef8
fabianlopezdev <fabianlopez@pm.me>
2024-03-06 10:01:02 +0100

    Changed netlify/functions for processing data in the same Astro Project. Already getting the list of seasons in the header

---
+++ src/data/seasonsCount.js
@@ -0,0 +1,40 @@
+// Import the necessary modules
+import fs from "fs";
+import { get } from "http";
+import path from "path";
+
+// Define the directory you want to inspect
+const directoryPath = path.join(process.cwd(), "src", "data","stats");
+
+// Read the directory contents
+const files = fs.readdirSync(directoryPath);
+
+// Count the number of files
+const seasonsCount = files.length -1;
+
+const firstSeason = +extractYearsFromString(files[0]);
+
+const listOfSeasons = getSeasonsList(seasonsCount, firstSeason);
+
+export function getSeasonsCount() {
+  return { seasonsCount, firstSeason, listOfSeasons }
+}
+
+// Example of iterating over the files
+function extractYearsFromString(inputString) {
+  // Regular expression to match the years pattern
+  let firstYear = inputString.split('-')[0];
+  let firstYearLastTwoDigits = firstYear.slice(-2);
+
+  return firstYearLastTwoDigits;
+}
+
+function getSeasonsList(seasonsCount, firstSeason) {
+  let currentSeason = firstSeason;
+  let seasonsList = [];
+  for (let i = 0; i < seasonsCount; i++) {
+    seasonsList.push(`${currentSeason}/${currentSeason + 1}`);
+    currentSeason++;
+  }
+  return seasonsList;
+}
---
+++ src/data/stats/stats.js
@@ -0,0 +1,92 @@
+// Import the necessary modules
+import XLSX from "xlsx";
+import path from "path";
+import { fileURLToPath } from "url"; 
+
+function removeAccents(str) {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+export const getSeasonStats = (season) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const workbook = XLSX.readFile(path.join(__dirname, `${season}_stats.ods`));
+
+
+
+  const sheetNames = workbook.SheetNames;
+
+  let resultats,
+    resultatsCopaDelRey,
+    classificacio,
+    jugadors,
+    entrenadors,
+    divisio,
+    classificacioAscens,
+    ascens,
+    promocio,
+    promocioTriangular;
+
+  sheetNames.forEach((sheetName) => {
+    const normalizedSheetName = removeAccents(sheetName).toLowerCase();
+
+    if (normalizedSheetName === "resultats") {
+      const sheet = workbook.Sheets[sheetName];
+      resultats = XLSX.utils.sheet_to_json(sheet);
+    }
+    if (normalizedSheetName === "resultats_copa_del_rey") {
+      const sheet = workbook.Sheets[sheetName];
+      resultatsCopaDelRey = XLSX.utils.sheet_to_json(sheet);
+    }
+    if (normalizedSheetName === "classificacio") {
+      const sheet = workbook.Sheets[sheetName];
+      classificacio = XLSX.utils.sheet_to_json(sheet);
+    }
+    if (normalizedSheetName === "jugadors") {
+      const sheet = workbook.Sheets[sheetName];
+      jugadors = XLSX.utils.sheet_to_json(sheet);
+    }
+    if (normalizedSheetName === "entrenadors") {
+      const sheet = workbook.Sheets[sheetName];
+      entrenadors = XLSX.utils.sheet_to_json(sheet);
+    }
+    if (normalizedSheetName === "divisio") {
+      const sheet = workbook.Sheets[sheetName];
+      divisio = XLSX.utils.sheet_to_json(sheet);
+    }
+    if (normalizedSheetName === "promocio") {
+      const sheet = workbook.Sheets[sheetName];
+      promocio = XLSX.utils.sheet_to_json(sheet);
+    }
+    if (normalizedSheetName === "promocio_triangular") {
+      const sheet = workbook.Sheets[sheetName];
+      promocioTriangular = XLSX.utils.sheet_to_json(sheet);
+    }
+
+    if (normalizedSheetName === "ascens") {
+      const sheet = workbook.Sheets[sheetName];
+      ascens = XLSX.utils.sheet_to_json(sheet);
+    }
+    if (normalizedSheetName === "classificacio_ascens") {
+      const sheet = workbook.Sheets[sheetName];
+      classificacioAscens = XLSX.utils.sheet_to_json(sheet);
+    }
+  });
+
+  const stats = {
+    ...(resultats !== undefined && { resultats }),
+    ...(resultatsCopaDelRey !== undefined && { resultatsCopaDelRey }),
+    ...(classificacio !== undefined && { classificacio }),
+    ...(jugadors !== undefined && { jugadors }),
+    ...(entrenadors !== undefined && { entrenadors }),
+    ...(divisio !== undefined && { divisio }),
+    ...(promocio !== undefined && { promocio }),
+    ...(promocioTriangular !== undefined && { promocioTriangular }),
+    ...(ascens !== undefined && { ascens }),
+    ...(classificacioAscens !== undefined && { classificacioAscens }),
+  };
+
+  return stats ;
+};
+
+
