3d7faa9cba13ac1ba6351a33587253715fbb12d6
fabianlopezdev <fabianlopez@pm.me>
2024-03-09 10:38:20 +0100

    Especials page: layout finished. Rendering especials articles dinamically

---
+++ src/data/especials.js
@@ -0,0 +1,47 @@
+// Import the necessary modules
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import mammoth from "mammoth";
+
+// When using ES modules, __dirname is not defined, so we define it manually
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//DEV PATH
+const folderPath = path.join(__dirname, `../../public/especials`);
+
+console.log("folderPath", folderPath);
+//PRODUCTION PATH
+// const folderPath = path.join(__dirname, "../estadistiques");
+
+// Read the directory contents
+const files = fs.readdirSync(folderPath);
+
+export async function getEspecials() {
+const especials = await Promise.all(
+  files.map(async (file) => {
+    let strQMark = file.replace('Pregunta', '?')
+    let strNoAuthor = strQMark.replace("Jordi_Sanuy_Bassa", "");
+
+    // Replace ".docx" with an empty string
+    let strNoExtension = strNoAuthor.replace(".docx", "");
+
+    let title = strNoExtension.split("_").join(" ");
+
+    const htmlValues = await mammoth.convertToHtml({
+      path: `${folderPath}/${file}`,
+    });
+
+    return {
+      title: title,
+      author: "Jordi Sanuy Bassa",
+      htmlValues: htmlValues.value,
+    };
+  }),
+);
+
+return especials;
+
+}
+
