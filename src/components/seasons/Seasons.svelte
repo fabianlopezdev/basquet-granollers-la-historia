499f5f55088dc81159cb3d3a8535357c06fe84df
fabianlopezdev <fabianlopez@pm.me>
2024-03-20 11:05:49 -0400

    Fixing the size of seasons container

--- src/components/seasons/Seasons.svelte
+++ src/components/seasons/Seasons.svelte
@@ -173,8 +173,8 @@ let isDialogOpen = false;
     --offset-season-menu: calc(var(--hg-menu) - 2px);
     background-color: var(--clr-contrast);
     position: relative;
-    min-height: 100vh;
-    height: 100%;
+    height: 100vh;
+    max-height: 100%;
     /* overflow-y: hidden; */
   }
 
